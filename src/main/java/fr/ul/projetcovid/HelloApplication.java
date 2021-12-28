74eec3e8320f58d93b228755b3a3d0fb8e89af33
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2021-12-28 14:40:49 +0100

    re-initialize project with Maven

---
+++ src/main/java/fr/ul/projetcovid/HelloApplication.java
@@ -0,0 +1,9 @@
+package fr.ul.projetcovid;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
